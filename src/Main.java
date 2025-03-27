92e93a2743b403b1ec61f375071fa31f631aea59
admin <m89606209505@yandex.ru>
2025-03-27 13:03:59 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
+        firstTask();
+        secondTask();
+    }
 
+    public static void firstTask() {
+        int savings = 0;
+        int monthlyContribution = 15000;
+        int target = 2459000;
+        int months = 0;
+
+        while (savings < target) {
+            months++;
+            savings += monthlyContribution;
+        }
+
+        System.out.println("Месяц " + months + ", сумма накоплений равна " + savings + " рублей");
+    }
+
+    public static void secondTask() {
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+
+        for (int j = 10; j >= 1; j--) {
+            System.out.print(j + " ");
+        }
     }
 }
+
+
+
+
+
+
+
+
+
