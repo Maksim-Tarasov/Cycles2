add1d2d24aa8af275b3346595492514b1c75b02c
admin <m89606209505@yandex.ru>
2025-03-29 08:29:33 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args) {
         firstTask();
         secondTask();
+        thirdTask();
+        fourthTask();
+        fifthTask();
+        sixthTask();
+        seventhTask();
+        eighthTask();
     }
 
     public static void firstTask() {
@@ -30,7 +36,80 @@ public class Main {
             System.out.print(j + " ");
         }
     }
+
+    public static void thirdTask() {
+        var population = 12_000_000;
+        var birthRate = 0.017;
+        var deathRate = 0.008;
+        int years = 10;
+        for (int year = 1; year <= years; year++) {
+            var growth = (birthRate - deathRate) * population;
+            population += growth;
+            System.out.println("Год " + year + ", численность населения составляет " + population);
+        }
+    }
+
+    public static void fourthTask() {
+        var savings = 15000;
+        var monthlyInterestRate = 0.07;
+        var target = 12_000_000;
+        int months = 0;
+
+        while (savings < target) {
+            months++;
+            savings *= (1 + monthlyInterestRate);
+            System.out.println("Месяц" + months + "сумма накоплений равна" + savings + "рублей");
+        }
+    }
+
+    public static void fifthTask() {
+        var savings = 15000;
+        var monthlyInterestRate = 0.07;
+        var target = 12_000_000;
+        int months = 0;
+
+        while (savings < target) {
+            months++;
+            savings *= (1 + monthlyInterestRate);
+            System.out.println("Месяц" + months + "сумма накоплений равна" + savings + "рублей");
+        }
+    }
+
+    public static void sixthTask() {
+        float sum = 15000f;
+        int countMonth = 0;
+        int years = 9;
+        while (countMonth < years * 12) {
+            countMonth++;
+            float per = (sum / 100) * 7;
+            sum += per;
+            if (countMonth % 6 == 0)
+                System.out.println(countMonth + ") сумма: " + (int) sum);
+        }
+        System.out.println("Всего " + countMonth + " месяцев, общая сумма: " + (int) sum + " рублей");
+
+    }
+
+    public static void seventhTask() {
+        int currentDay = 7 + 1;
+        while (currentDay < 31) {
+            System.out.println("Сегодня пятница," + currentDay + "-е число. Необходимо подготовить отчет");
+            currentDay += 7;
+        }
+    }
+
+    public static void eighthTask() {
+        int tmp = 79;
+        int minYear = 1825;
+        int maxYear = 2125;
+        for (int i = minYear; i < maxYear; i++) {
+            if (i % tmp == 0)
+                System.out.println(i);
+        }
+    }
 }
+
+
 
 
 
